837f945285e5d61edaf4dd005526ad2e01dcfbd3
hajduty <hajduty@outlook.com>
2026-02-07 15:28:17 +0100

    tests

---
+++ JobTracker.Tests/DbFactory.cs
@@ -0,0 +1,21 @@
+﻿using JobTracker.Application.Infrastructure.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace JobTracker.Tests;
+
+public static class DbFactory
+{
+    public static IDbContextFactory<AppDbContext> CreateDbFactory()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(connection)
+            .Options;
+        var context = new AppDbContext(options);
+        context.Database.EnsureCreated();
+        return new PooledDbContextFactory<AppDbContext>(options);
+    }
+}
---
+++ JobTracker.Tests/JobSearchTests.cs
@@ -0,0 +1,60 @@
+using JobTracker.Application.Features.JobSearch.GetJobs;
+using JobTracker.Application.Features.JobSearch.GetJobTitles;
+using JobTracker.Application.Features.JobSearch.LoadJobs;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace JobTracker.Tests;
+
+// fast tests, not testing the entire flow, but just that we can fetch job titles, load jobs and fetch jobs from db
+public class JobSearchTests
+{
+    [Fact]
+    public async Task GetJobTitlesAsync()
+    {
+        var sut = new GetJobTitles();
+        var result = await sut.ExecuteAsync(new GetJobTitlesRequest("software"));
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task LoadJobsAsync()
+    {
+        var dbFactory = DbFactory.CreateDbFactory();
+
+        var sut = new LoadJobs(dbFactory);
+
+        // Act
+        await sut.ExecuteAsync(new LoadJobsRequest("utvecklare"));
+
+        await using var db = await dbFactory.CreateDbContextAsync();
+        var jobs = await db.Postings.ToListAsync();
+
+        Debug.WriteLine("Total jobs in db: " + jobs.Count);
+
+        Assert.NotEmpty(jobs);
+    }
+
+    [Fact]
+    public async Task GetJobsAsync()
+    {
+        var dbFactory = DbFactory.CreateDbFactory();
+        var sut = new GetJobs(dbFactory);
+
+        var sut2 = new LoadJobs(dbFactory);
+
+        // Act
+        await sut2.ExecuteAsync(new LoadJobsRequest("utvecklare"));
+
+        await Task.Delay(500);
+
+        // Act
+        var result = await sut.ExecuteAsync(new GetJobsRequest("utvecklare", 1, 10, new List<int>()));
+
+        Console.WriteLine($"Total results: {result.TotalResults}, Total pages: {result.TotalPages}");
+        Debug.WriteLine($"Total results: {result.TotalResults}, Total pages: {result.TotalPages}");
+
+        Assert.NotNull(result);
+        Assert.NotEmpty(result.Postings);
+    }
+}
